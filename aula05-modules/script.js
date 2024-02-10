893bdd71b00cbfd67b286a57cb0ba3fe9176c55c
brunafernxndes <brunafernandess@outlook.com.br>
2024-02-10 17:51:28 -0300

    first commit

---
+++ aula05-modules/script.js
@@ -0,0 +1,12 @@
+import { areaQuadrado, perimetroQuadrado } from './quadrado.js';
+import numeroAleatorio from './numeroAleatorio.js';
+import circulo from './circulo.js';
+
+console.log(areaQuadrado(4));
+console.log(perimetroQuadrado(5));
+
+console.log(numeroAleatorio());
+
+console.log(circulo.area(5));
+console.log(circulo.circunferencia(5));
+console.log(circulo.aleatorio());
